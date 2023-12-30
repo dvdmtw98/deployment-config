0d0aae9dbccbb281d43dc691698223a8d634203a
David Varghese <dvdmtw99@gmail.com>
2023-12-30 10:21:55 -0600

    Feat: Improve Processing Logic & Added Jekyll Callout Support

--- modify_links.py
+++ modify_links.py
@@ -1,137 +1,22 @@
 '''
-Script to modify Markdown Links in Markdown Files
+Script to modify Image Links, Outgoing Links & Callouts in Markdown Files
 
-References:
+Markdown Link Regex:
 https://davidwells.io/snippets/regex-match-markdown-links
 https://stephencharlesweiss.com/regex-markdown-link
+
+Kramdown Syntax:
 https://kramdown.gettalong.org/syntax.html
+
+Callout Block Regex:
+https://github.com/sondregronas/mkdocs-callouts/blob/main/src/mkdocs_callouts/utils.py
+https://github.com/Pseudonium/Obsidian_to_Anki/issues/332
 '''
 
 from __future__ import annotations
-from typing import TextIO
 import re
 import os
 import glob
-
-
-def perform_file_transformation(
-    regex_pattern: str, source_filepath: str,
-    common_image_extensions: tuple[str], common_outgoing_extensions: tuple[str],
-    site_generator: str
-) -> None:
-    '''
-    Function to call the required transformation function
-    '''
-
-    destination_filepath = source_filepath.rstrip('.md') + ' Temp.md'
-
-    # This function is only executed on the Main Index file
-    if source_filepath.find("Main Index") != -1:
-        with open(source_filepath, "r", encoding='utf-8') as input_file, \
-                open(destination_filepath, "w", encoding='utf-8') as output_file:
-
-            process_main_index_content(input_file, output_file, regex_pattern)
-
-        os.replace(destination_filepath, source_filepath)
-
-    if source_filepath.find("Various Lists") == -1:
-        # This function is used to process the image links that are present in the file
-        with open(source_filepath, "r", encoding='utf-8') as input_file, \
-                open(destination_filepath, "w", encoding='utf-8') as output_file:
-
-            process_file_image_links(input_file, output_file, regex_pattern, common_image_extensions)
-
-        os.replace(destination_filepath, source_filepath)
-
-        # This function is used to process the hyperlinks that are present in the file
-        with open(source_filepath, "r", encoding='utf-8') as input_file, \
-                open(destination_filepath, "w", encoding='utf-8') as output_file:
-
-            process_file_outgoing_links(
-                input_file, output_file, regex_pattern, common_outgoing_extensions, site_generator)
-
-        os.replace(destination_filepath, source_filepath)
-
-
-def process_file_outgoing_links(
-    input_file: TextIO, output_file: TextIO,
-    regex_pattern: str, common_link_extensions: tuple[str],
-    site_generator: str
-) -> None:
-    '''
-    Function to modify the hyperlinks that are present in the files
-    '''
-
-    for line in input_file:
-        search_result = re.search(regex_pattern, line.rstrip('\n'))
-
-        if (
-            search_result and not search_result.group(0).startswith('!') and
-            search_result.group(2).startswith(common_link_extensions)
-        ):
-            # print(search_result.group(0))
-            match_end_index = search_result.end()
-
-            if site_generator == 'mkdocs':
-                kramdown_attributes = '{: target="_blank" rel="noopener noreferrer" style="text-decoration:underline" }'
-            else:
-                kramdown_attributes = '{: target="_blank" rel="noopener noreferrer" }'
-
-            if line.find('{:', match_end_index, match_end_index + 2) == -1:
-                new_link_format = search_result.group(0) + kramdown_attributes
-                modified_line = re.sub(regex_pattern, new_link_format, line)
-                output_file.write(modified_line)
-            else:
-                output_file.write(line)
-
-        else:
-            output_file.write(line)
-
-
-def process_main_index_content(
-    input_file: TextIO, output_file: TextIO, regex_pattern: str
-) -> None:
-    '''
-    Function to remove Various Lists from Index
-    '''
-
-    for line in input_file:
-        search_result = re.search(regex_pattern, line.rstrip('\n'))
-
-        if search_result:
-            if search_result.group(1) not in ["Read & Watch List", "Languages"]:
-                output_file.write(line)
-        else:
-            output_file.write(line)
-
-
-def process_file_image_links(
-    input_file: TextIO, output_file: TextIO,
-    regex_pattern: str, common_image_extensions: tuple[str]
-) -> None:
-    '''
-    Function to Convert Markdown Image links to Mkdocs Image Links
-    '''
-
-    for line in input_file:
-        search_result = re.search(regex_pattern, line.rstrip('\n'))
-
-        if (
-            search_result and search_result.group(0).startswith('!') and
-            search_result.group(2).endswith(common_image_extensions)
-        ):
-            # print(search_result.group(0))
-            description_part = search_result.group(1).rsplit('|')
-
-            if len(description_part) > 1:
-                output_file.write(
-                    f'![{description_part[0]}]({search_result.group(2)})'
-                    f'{{: style="width:{description_part[1]}px" }}\n'
-                )
-            else:
-                output_file.write(line)
-        else:
-            output_file.write(line)
 
 
 def source_directory_selector(**kwargs: dict[str, dict | list]) -> tuple[str, str]:
@@ -150,32 +35,162 @@ def source_directory_selector(**kwargs: dict[str, dict | list]) -> tuple[str, st
     return source_directory, site_generator
 
 
+def perform_file_transformation(
+    source_filepath: str, site_generator: str,
+    link_regex_pattern: str, callout_regex_pattern: str
+) -> None:
+    '''
+    Main function to call the transformation logic
+    '''
+
+    with open(source_filepath, "r", encoding='utf-8') as input_file:
+        file_content = input_file.read()
+
+    # Process Index file
+    markdown_filename = os.path.basename(os.path.splitext(source_filepath)[0])
+    if markdown_filename in ["Main Index"]:
+        file_content = process_main_index(file_content, link_regex_pattern)
+
+    # Process Links
+    links_from_file = re.finditer(link_regex_pattern, file_content, flags=re.I)
+    for link in links_from_file:
+        if link.group(0).startswith("!["):
+            file_content = process_images(file_content, link, site_generator)
+        else:
+            file_content = process_outgoing_links(file_content, link, site_generator)
+
+    # Process Callouts
+    callout_mapping = {
+        'tip': ['tip', 'hint', 'important'],
+        'info': ['info', 'note'],
+        'warning': ['warning', 'caution', 'attention'],
+        'danger': ['danger', 'error']
+    }
+
+    callouts_from_file = re.finditer(callout_regex_pattern, file_content, flags=re.I | re.M)
+    for callout in callouts_from_file:
+        file_content = process_callouts(file_content, callout, callout_mapping, site_generator)
+
+    with open(source_filepath, "w", encoding='utf-8') as output_file:
+        output_file.write(file_content)
+
+
+def process_main_index(file_content: str, link_regex_pattern: str) -> str:
+    '''
+    Function to remove entries from Index file
+    '''
+
+    accumulated_file = ''
+    for line in file_content.splitlines():
+        search_result = re.search(link_regex_pattern, line.rstrip('\n'))
+        if search_result:
+            if search_result.group(2) not in ["Read & Watch List", "Languages"]:
+                accumulated_file += f'{line}\n'
+        else:
+            accumulated_file += f'{line}\n'
+
+    return accumulated_file
+
+
+def process_callouts(
+    file_content: str, callout_match: re.Match,
+    callout_mapping: dict[str, list[str]], site_generator: str
+) -> str:
+    '''
+    Function to modify callouts
+    '''
+
+    if callout_match.group(5) or site_generator == "mkdocs":
+        return file_content
+
+    callout_type = callout_match.group(2).lower()
+    callout_title = callout_match.group(3).strip() if callout_match.group(3).strip() else ''
+    callout_body = callout_match.group(4)
+
+    kramdown_type = None
+    for chirpy_type, obsidian_types in callout_mapping.items():
+        if callout_type in obsidian_types:
+            kramdown_type = chirpy_type
+    kramdown_type = kramdown_type if kramdown_type else 'info'
+    kramdown_attributes = f'{{: .prompt-{kramdown_type} }}'
+
+    if callout_title:
+        modified_callout = f'> **{callout_title}**  \n{callout_body}\n{kramdown_attributes}'
+    else:
+        modified_callout = f'{callout_body}\n{kramdown_attributes}'
+
+    original_callout = callout_match.group(1)
+    file_content = file_content.replace(original_callout, modified_callout)
+    return file_content
+
+
+def process_outgoing_links(file_content: str, link_match: re.Match, site_generator: str) -> str:
+    '''
+    Function to modify the outgoing hyperlinks
+    '''
+
+    original_link = link_match.group(1)
+
+    if link_match.group(5) or not link_match.group(3).startswith(("http", "https")):
+        return file_content
+
+    if site_generator == 'mkdocs':
+        kramdown_attributes = '{: target="_blank" rel="noopener noreferrer" style="text-decoration:underline" }'
+    else:
+        kramdown_attributes = '{: target="_blank" rel="noopener noreferrer" }'
+
+    description = link_match.group(2).replace("|", r"\|")
+    outgoing_link = link_match.group(3)
+
+    modified_link = f'[{description}]({outgoing_link}){kramdown_attributes}'
+
+    file_content = file_content.replace(original_link, modified_link)
+    return file_content
+
+
+def process_images(file_content: str, image_match: re.Match, site_generator: str) -> str:
+    '''
+    Function to Convert Markdown Image links to Kramdown Image Links
+    '''
+
+    if image_match.group(5):
+        return file_content
+
+    description = image_match.group(2).rsplit('|')
+    image_link = image_match.group(3)
+
+    if len(description) > 1:
+        if site_generator == "mkdocs":
+            modified_image = f'![{description[0]}]({image_link}){{: style="width:{description[1]}px" }}'
+        else:
+            modified_image = f'![{description[0]}]({image_link}){{: width="{description[1]}" .shadow }}'
+    else:
+        if site_generator == "mkdocs":
+            modified_image = f'![{description[0]}]({image_link}){{: style="width:640px" }}'
+        else:
+            modified_image = f'![{description[0]}]({image_link}){{: width="640" .shadow }}'
+
+    original_image = image_match.group(1)
+    file_content = file_content.replace(original_image, modified_image)
+    return file_content
+
+
 def on_pre_build(**kwargs) -> None:
     '''
-    Main driver function code
+    Main driver function
     '''
 
     print('Starting: "modify_links" script...')
 
-    # Regex to select Markdown Links
-    regex_pattern = r'!?\[([^\]]*)?\]\(((https?:\/\/)?[A-Za-z0-9\:\/\. %&-_]+)(\"(.+)\")?\)'
-    common_image_extensions = ('.png', '.gif', '.jpg', '.jpeg', '.webp')
-    common_link_extensions = ('http://', 'https://')
+    links_regex_pattern = r'(!?\[([^\]]*)?\]\(((?:https?://)?[A-Za-z0-9:/.%&#-_ ]+)(?:"(.+)")?\))({:(?:.+)})?'
+    callout_regex_pattern = r'((?:>+) *\[!([^\]]*)\](.*)?\n(.+(?:\n(?:^.{1,3}$|^.{4}(?<!<!--).*))*))({:(?:.+)})?'
 
     source_directory, site_generator = source_directory_selector(**kwargs)
-    # print(source_directory)
-    source_file_path = f'{source_directory}/**/*.md'
-    # print(source_file_path)
+    source_file_path = os.path.join(f'{source_directory}', '**', '*.md')
 
     for filepath in glob.iglob(source_file_path, recursive=True):
-
-        normalized_filepath = filepath.replace('\\', '/')
-        print(normalized_filepath)
-
-        perform_file_transformation(
-            regex_pattern, normalized_filepath, common_image_extensions,
-            common_link_extensions, site_generator
-        )
+        print(filepath)
+        perform_file_transformation(filepath, site_generator, links_regex_pattern, callout_regex_pattern)
 
     print('Completed: "modify_links" script...')
 
